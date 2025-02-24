97511e3dbedc5cbc06e3d92d18a9b76f7e8a3a9a
Aman Pal <amannathpal@gmail.com>
2025-02-24 22:30:42 +0530

    one

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:5173",
-      "hhttps://e-shop-six-dusky-38.vercel.app",
+      "https://e-shop-six-dusky-38.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
