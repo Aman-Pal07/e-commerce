71d1aa54c0e4e09957087a24a13d0b582cefbb34
Aman Pal <amannathpal@gmail.com>
2025-02-26 15:14:03 +0530

    done error

--- backend/app.js
+++ backend/app.js
@@ -13,9 +13,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:5173",
-      "https://e-shop-six-dusky-38.vercel.app",
-      "https://e-shop-git-main-aman-creations-projects.vercel.app",
-      "https://e-shop-oxfdcxe8b-aman-creations-projects.vercel.app",
+      "https://e-shop-1-v07y.onrender.com",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,1 +1,1 @@
-export const server = "http://localhost:8080/api/v2";
+export const server = "https://e-commerce-backend-z4g6.onrender.com/api/v2";
